b9f6a3219ce3d4964f6d4f99585da1df9a1085ac
AkbharChowdhury <112480476+AkbharChowdhury@users.noreply.github.com>
2024-01-30 18:01:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from colour import Colours
+from schedule import Schedule
+FONT_NAME = "Courier"
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+"""
+This 
+"""
+def main():
+   print(Colours.PINK.value)
+   print([(e.value, e.name) for e in Colours])
+
+
+if __name__ == '__main__':
+    main()
---
+++ schedule.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Schedule(Enum):
+    WORK_MIN = 25
+    SHORT_BREAK_MIN = 5
+    LONG_BREAK_MIN = 20
