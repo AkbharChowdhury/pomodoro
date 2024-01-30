632cd79e64c4032c45ddc640d23f24c014aaf8ea
AkbharChowdhury <112480476+AkbharChowdhury@users.noreply.github.com>
2024-01-30 18:26:34 +0000

    x

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from colour import Colours
 from schedule import Schedule
+from tkinter import *
+
 FONT_NAME = "Courier"
 
 
@@ -9,13 +11,17 @@ FONT_NAME = "Courier"
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
-"""
-This 
-"""
+
 def main():
-   print(Colours.PINK.value)
-   print([(e.value, e.name) for e in Colours])
+    window = Tk()
+    window.title('pomodoro'.title())
+    window.config(padx=100, pady=50)
+    canvas = Canvas(width=200, height=224)
+    tomato_image = PhotoImage(file='tomato.png')
+    canvas.create_image(103, 112, image=tomato_image)
+    canvas.create_text(103, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white')
+    canvas.pack()
+    window.mainloop()
 
 
 if __name__ == '__main__':
